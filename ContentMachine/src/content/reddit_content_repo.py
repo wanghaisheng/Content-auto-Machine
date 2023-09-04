f94f568204f2eb760a7ca5a00135b54c1fb86703
Adrian Mohnacs <amohnacs@gmail.com>
2023-09-04 19:18:27 +0700

    Python comments for copyright

--- ContentMachine/src/content/reddit_content_repo.py
+++ ContentMachine/src/content/reddit_content_repo.py
@@ -1,3 +1,11 @@
+"""
+Content Machine Complete
+Version: 1.0.0
+Author: Adrian Mohnacs
+Copyright (c) 2023 Adrian Mohnacs
+All rights reserved. Unauthorized copying or reproduction of this file is prohibited.
+"""
+
 import appsecrets
 import praw
 
--- ContentMachine/src/utility/time_utils.py
+++ ContentMachine/src/utility/time_utils.py
@@ -1,3 +1,11 @@
+"""
+Content Machine Complete
+Version: 1.0.0
+Author: Adrian Mohnacs
+Copyright (c) 2023 Adrian Mohnacs
+All rights reserved. Unauthorized copying or reproduction of this file is prohibited.
+"""
+
 from datetime import datetime, timedelta
 import utility.time_utils as time_utils
 
